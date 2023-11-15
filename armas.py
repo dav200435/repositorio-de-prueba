b777d5a80920035e1cf4667233563b5f9926c172
dav200435 <davacas2005@gmail.com>
2023-11-15 19:12:19 +0100

    se sube el codigo inicial

---
+++ armas.py
@@ -0,0 +1,26 @@
+class armas:
+    def __init__(self,range,cadencia,damage):
+        self.range=range
+        self.cadencia=cadencia
+        self.damage=damage
+    
+    def disparar(self):
+        if buttomshoot = True:
+            bullet.calculate(self.range,self.cadencia,self.damage)
+            
+pistola = armas(20,2,12)
+fusil = armas(40,2.5,17)
+snipper = armas(90,1,35)
+pistola.disparar()
+
+class bullet:
+    def __init__(self,range,cadencia,damage):
+        self.range=range
+        self.cadencia=cadencia
+        self.damage=damage
+        
+    def calculate(self,range,cadencia,damage):
+        range*map
+        cadencia=tic*self.cadencia
+        damage=1*self.damage
+
